506cc8c8a7c02ebaadb680fd236d10c9ae2ef009
Mathias Jean Johansen <mathias@mjj.io>
2020-08-07 16:25:17 +0200

    Update formula for `voxctl`.

--- Formula/voxctl.rb
+++ Formula/voxctl.rb
@@ -1,11 +1,12 @@
 class Voxctl < Formula
   desc "Command-line tool for controlling the VOX music player"
   homepage "https://github.com/majjoha/voxctl"
-  url "https://github.com/majjoha/voxctl/archive/v1.0.0.tar.gz"
-  sha256 "4fcc5936b1378793dc6c7494169cf84101a42e2a2a6e5760b5113fc904ea1f20"
-  version "1.0.0"
+  url "https://github.com/majjoha/voxctl/archive/v1.1.0.tar.gz"
+  sha256 "f26d7377dcdfeff9e7e0a807dcbfff6e90631a83832a9e35561e7be910a8c0c4"
+  version "1.1.0"
 
   def install
     bin.install "voxctl"
+    zsh_completion.install "share/zsh/site-functions/_voxctl" => "_voxctl"
   end
 end
