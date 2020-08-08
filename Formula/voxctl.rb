a27ad00106cfbddbb36c70da0649594653884db5
Dario Vladovic <d.vladimyr@gmail.com>
2020-08-08 16:22:10 +0200

    Update voxctl formula

--- Formula/voxctl.rb
+++ Formula/voxctl.rb
@@ -3,10 +3,13 @@ class Voxctl < Formula
   homepage "https://github.com/majjoha/voxctl"
   url "https://github.com/majjoha/voxctl/archive/v1.1.0.tar.gz"
   sha256 "f26d7377dcdfeff9e7e0a807dcbfff6e90631a83832a9e35561e7be910a8c0c4"
-  version "1.1.0"
+  license "ISC"
+  head "https://github.com/majjoha/voxctl.git"
+
+  bottle :uneeded
 
   def install
     bin.install "voxctl"
-    zsh_completion.install "share/zsh/site-functions/_voxctl" => "_voxctl"
+    zsh_completion.install "contrib/completion/_voxctl"
   end
 end
