cbbdf46348375e070146d9718084854a5eb7f4a3
Mathias Jean Johansen <mathias@mjj.io>
2020-08-10 11:32:03 +0200

    Bump `voxctl` version.

--- Formula/voxctl.rb
+++ Formula/voxctl.rb
@@ -1,8 +1,9 @@
 class Voxctl < Formula
   desc "Command-line tool for controlling the VOX music player"
   homepage "https://github.com/majjoha/voxctl"
-  url "https://github.com/majjoha/voxctl/archive/v1.1.0.tar.gz"
-  sha256 "f26d7377dcdfeff9e7e0a807dcbfff6e90631a83832a9e35561e7be910a8c0c4"
+  url "https://github.com/majjoha/voxctl/archive/v1.1.1.tar.gz"
+  sha256 "2555a9ea6c74957b90b411df2d22ace1c62e09d7a13e2992f3c2f1524e3a143b"
+  version "1.1.1"
   license "ISC"
   head "https://github.com/majjoha/voxctl.git"
 
